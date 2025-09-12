80a6cf1c0ff11b3bb07b96968d822d2715033fb2
Numsik <honzik.klusek.myto@gmail.com>
2025-09-12 16:04:51 +0200

    funguje zmena

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 
 async function getapi(city){
-    let response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Pardubice?unitGroup=uk&key=LHN49XJPS5H5JJ5N64RMQQT7C&contentType=json")
+    let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=uk&key=LHN49XJPS5H5JJ5N64RMQQT7C&contentType=json`)
     let data = await response.json();
     console.log(response)
     
