3d2be88db8e18db0727c6fc24a3c6c0342f98583
Numsik <honzik.klusek.myto@gmail.com>
2025-09-12 13:09:15 +0200

    start of js

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+
+async function getapi(city){
+    let response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Pardubice?unitGroup=uk&key=LHN49XJPS5H5JJ5N64RMQQT7C&contentType=json")
+    let data = await response.json();
+    console.log(response)
+    
+    
+    let adress = data.address
+    console.log(adress)
+   
+
+}
+getapi("Pardubice");
+
+document.querySelector('.send').addEventListener('click', () =>{
+    const input = document.querySelector('.nameofcity')
+    const nameofcity = input.value
+    getapi(nameofcity);
+});
